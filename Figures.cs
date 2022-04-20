a7300991ca3f2fed016f962bd07ac2b206d624c2
flyntic <64376713+flyntic@users.noreply.github.com>
2022-04-20 21:46:01 +0300

    Add files via upload

---
+++ Figures.cs
@@ -0,0 +1,186 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace PrZadanie;
+
+public abstract class Figure
+{
+    public double Square { get; set; }
+    public double Perimetr { get; set; }
+}
+
+public class Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString()
+    {
+        return $"X:{X} Y:{Y}";
+    }
+    
+}
+
+public interface N_angle
+{
+    Point[] Points { get; set; }
+    int CountPoints();
+    double Height();
+    double Base();
+    double AngleBaseAdjSide();
+    double CountSides();
+    double[] LengthSide();
+
+}
+
+public class Triangle : Figure,N_angle
+{
+
+    private const int countPoints = 3;
+    public Point[] Points { get; set; }
+
+    public int CountPoints()
+    {
+        return countPoints;
+    }
+
+    public double Height()
+    {
+        return 100;
+    }
+
+    public double Base()
+    {
+        return 200;
+    }
+
+    public double AngleBaseAdjSide()
+    {
+        return 90;
+    }
+
+    public double CountSides()
+    {
+        return countPoints;
+    }
+
+    public double[] LengthSide()
+    {
+        return new double[countPoints] {3, 4, 5};
+    }
+
+    public override string ToString()
+    {
+        return $"Треугольник";
+    }
+}
+
+
+public class FourAngle : Figure,N_angle
+{
+
+    private const int countPoints = 4;
+    public Point[] Points { get; set; }
+
+    public int CountPoints()
+    {
+        return countPoints;
+    }
+
+    public double Height()
+    {
+        return 100;
+    }
+
+    public double Base()
+    {
+        return 200;
+    }
+
+    public double AngleBaseAdjSide()
+    {
+        return 90;
+    }
+
+    public double CountSides()
+    {
+        return countPoints;
+    }
+
+    public double[] LengthSide()
+    {
+        return new double[countPoints] {3, 4, 5, 6};
+    }
+
+    public override string ToString()
+    {
+        return $"Четырехугольник";
+    }
+    
+    //public static implicit operator FourAngle (Square S)
+    //{
+    //    return new Square() ;
+    //}
+    
+}
+
+public class Square : FourAngle
+{
+    public bool isSquare()
+    {
+        if (Points[0].X == 0) return true;
+        return false;
+    }
+
+    //public static implicit operator Square (FourAngle S)
+    //{
+    //    return new Square() ;
+    //}
+
+    
+    public override string ToString()
+    {
+        return "Квадрат";
+    }
+    
+}
+
+public class Rhomb : FourAngle
+{
+    public bool isRhomb()
+    {
+        if (Points[0].X == 0) return true;
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Ромб";
+    }
+}
+
+public class Rectangle : FourAngle
+{
+    public bool isRectangle()
+    {
+        if (Points[0].X == 0) return true;
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Прямоугольник";
+    }
+}
+public class Parallelogram : FourAngle
+{
+    public bool isParallelogram()
+    {
+        if (Points[0].X == 0) return true;
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Параллелограм";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using PrZadanie;
+
+
+N_angle[] n_angles = new N_angle[]
+{
+    new Triangle()
+    {
+        Points = new Point[] {new Point {X = 2, Y = 0}, new Point {X = 3, Y = 0}, new Point {X = 4, Y = 10}}
+    },
+    new FourAngle()
+    {
+        Points = new Point[]
+            {new Point {X = 2, Y = 0}, new Point {X = 3, Y = 0}, new Point {X = 4, Y = 10}, new Point {X = 4, Y = 13}}
+
+    },
+    new FourAngle()
+    {
+        Points = new Point[]
+            {new Point {X = 2, Y = 0}, new Point {X = 3, Y = 0}, new Point {X = 4, Y = 10}, new Point {X = 4, Y = 13}}
+
+    },
+    new Triangle()
+    {
+        Points = new Point[] {new Point {X = 2, Y = 0}, new Point {X = 3, Y = 0}, new Point {X = 4, Y = 10}}
+
+    }
+};
+
+
+foreach (N_angle n in n_angles)
+{
+    //Square t = n;
+    if (n as FourAngle != null)
+    {
+        FourAngle t = FourAngle(n);
+        Square z = t;
+         if (z.isSquare()) Console.WriteLine(z);
+    }
+}
+   
+   
+   
+   
